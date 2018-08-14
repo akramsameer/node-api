6888f668eec4aac7235f3f83a6b4e50d0f836f77
Akram <akram.sameer99@gmail.com>
2018-08-14 20:51:32 +0200

    deploy to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://akram.samir:as012451245@ds221242.mlab.com:21242/todoapp231');
 
 module.exports={
     mongoose
